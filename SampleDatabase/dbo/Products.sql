e18ab7c27742dcc444bebdf391bb97ab2eb6a2e3
Rajesh Muraleedharan <16232384+rajeshmuraleedharan@users.noreply.github.com>
2019-03-10 01:45:23 -0600

    added post deployment script

---
+++ SampleDatabase/dbo/Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	[Name] VARCHAR(40) NOT NULL, 
+    [UnitPrice] MONEY NOT NULL, 
+    [UnitsInStock] INT NOT NULL, 
+	[SupplierId] INT NOT NULL,
+    CONSTRAINT [FK_Products_ToTable] FOREIGN KEY ([SupplierId]) REFERENCES [Suppliers]([Id])
+)
---
+++ SampleDatabase/dbo/Suppliers.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Suppliers]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1), 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Address] VARCHAR(50) NOT NULL
+)
